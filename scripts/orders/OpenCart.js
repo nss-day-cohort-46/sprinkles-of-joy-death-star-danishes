569a13abe4f939f9b932b89a1d5886335ecf410e
dtracydepp <tracydepp82@gmail.com>
2021-02-23 15:18:51 -0600

    When the user clicks on an order hyperlink, then the user will be presented with the order,detail,including all the products for that order with order status, total, and timestamp

--- scripts/orders/OpenCart.js
+++ scripts/orders/OpenCart.js
@@ -16,6 +16,11 @@ const render = () => {
   let cartHTML = ""
   let totalCost = 0
 
+  let orderButton = `<button id="placeOrder">Place Order</button>`
+    if (productsInCart.length === 0) {
+      orderButton = `<button id="placeOrder" disabled >Place Order</button>`
+    }
+
   for (const product of productsInCart) {
     cartHTML += `
       <div class="cart">
@@ -32,7 +37,7 @@ const render = () => {
     ${cartHTML}
     <hr/>
     <div class="cart">
-    <button id="placeOrder">Place Order</button>
+    ${orderButton}
     <p>$${totalCost.toFixed(2)}</p>
     </div>
     </div>
@@ -42,6 +47,7 @@ const render = () => {
 eventHub.addEventListener("showCustomerCart", e => OpenCart())
 
 eventHub.addEventListener("addToCart", evt => {
+  // console.log("addtoCart",evt)
   const productId = evt.detail.addedProduct
   getProducts()
     .then(() => {
@@ -52,8 +58,9 @@ eventHub.addEventListener("addToCart", evt => {
     })
 })
 
-eventHub.addEventListener("click", clickEvent => {
-  if (clickEvent.target.id === "placeOrder" && productsInCart.length !== 0) {
+userCart.addEventListener("click", clickEvent => {
+  // console.log("placeOrder",clickEvent)
+  if (clickEvent.target.id === "placeOrder" && productsInCart.length !== 0 && authHelper.isUserLoggedIn()) {
     const currentCustomerId = parseInt(authHelper.getCurrentUserId())
     getStatuses()
       .then(() => {
@@ -67,6 +74,11 @@ eventHub.addEventListener("click", clickEvent => {
         }
 
         return saveOrder(newOrder, productsInCart)
+        
+      })
+      .then(() => {
+        productsInCart = []
+        OpenCart ()
       })
   }
 })
